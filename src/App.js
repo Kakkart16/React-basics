833ae849b5529b8b8720904502d340a165f04737
Kakkart16 <kakkartushar.16@gmail.com>
2024-03-27 18:45:36 +0530

    added random color generator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
-      <Accordian/>
+
+      {/* Accordian Component */}
+      {/* <Accordian/> */}
+
+      {/* Random Color Component */}
+      <RandomColor/>
+
+
     </div>
   );
 }
