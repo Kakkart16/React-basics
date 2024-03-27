9643d3d3d3da35d47507e29b058d171db22013ba
asus <kakkartushar.16@gmail.com>
2024-03-27 11:51:54 +0530

    initial commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: "How are u?",
+        answer: "fine",
+    },
+    {
+        id: '2',
+        question: "How is your family?",
+        answer: "They are doing good",
+    },
+    {
+        id: '3',
+        question: "Tell me somrthing about yourself?",
+        answer: "I am a competitive programmer",
+    },
+    {
+        id: '4',
+        question: "Hope you are enjoying react?",
+        answer: "Yes, I am enjoying it",
+    },
+];
+
+export default data;
